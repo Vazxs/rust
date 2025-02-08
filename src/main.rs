2c4d7a62a86730c7b8db41ba0e07555428f3a0e9
vazxs <vs2090117@gmail.com>
2025-02-08 22:21:59 +0530

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+
+    let number:i32= 6;
+
+    match number {
+        1 => println!(" value is one."),
+        2 => println!(" value is two."),
+        3..=8 => println!(" value is b/w 3 to 8."),
+        _=> println!("default value."),
+    }
 }
